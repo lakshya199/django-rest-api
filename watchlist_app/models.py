df1a8f5f732e12f59540f1a06eee13f6466cc0c3
lakshya199 <61405053+lakshya199@users.noreply.github.com>
2022-09-07 19:13:57 +0530

    drf

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# class StreamPlatform(models.Model):
+#     rating = models.IntegerField(default=0)
+#     Directorname = models.CharField(max_length=150)
+#     website = models.URLField(max_length=100)
+
+#     def __str__(self):
+#         return self.rating
+
 class Movie(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
     
+    name = models.CharField(max_length=30)
+    Description = models.CharField(max_length=150)
+    active = models.BooleanField(default=True)
+
+   
     def __str__(self):
         return self.name
